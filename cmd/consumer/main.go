3aad6513b4f5be76eab2b414fa5f02a094d42375
GabiHert <hertergabriel4@gmail.com>
2024-09-12 08:14:41 -0300

    wip

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -4,11 +4,20 @@ import (
 	"context"
 	"log/slog"
 
+	"github.com/joho/godotenv"
 	"github.com/trend-me/ai-requester/internal/config/injector"
 	"github.com/trend-me/ai-requester/internal/integration/connections"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		slog.Error("Error loading .env file",
+			slog.String("error", err.Error()),
+		)
+		return
+	}
+
 	ctx := context.Background()
 	consumer, err := injector.InitializeQueueAiRequesterConsumer()
 	if err != nil {
@@ -18,7 +27,10 @@ func main() {
 		return
 	}
 
-	_, _ = consumer.Consume(ctx)
-
 	defer connections.Disconnect()
+
+	for {
+		_, _ = consumer.Consume(ctx)
+	}
+
 }
